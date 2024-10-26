c242c4f13458493b9c679970c90b2597894989cf
Manikantasammeta <manikantasammeta999@gmail.com>
2024-10-26 14:32:22 +0530

    yes

--- list_based2.py
+++ list_based2.py
@@ -1,3 +1,11 @@
+
+#                ALL THE PYTHON CODE ARE COMMENTED. IF YOU WANT TO RUN AND EXECUTE THE CODE,JUST UNCOMMENT THE CODE. 
+#                THE RESPECTIVE OUTPUT WILL BE DISPLAYED AT THE BOTTOM OF THE CODE. CHECK IT ONCE.
+
+
+
+
+
 # L=["rama","raju","raghu","mani","sita"]
 
 # print([ i[0].upper()+":"+i for i in L])
