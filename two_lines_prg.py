84ca1d15f82182385296a1b3fa51e83b0d05ea3a
Manikantasammeta <manikantasammeta999@gmail.com>
2024-11-05 18:12:30 +0530

    Done

--- two_lines_prg.py
+++ two_lines_prg.py
@@ -6,18 +6,12 @@
 # n=int(input("enter a nuber: "))
 # print("prime" if (n%i!=0 for i in range(2,n)) and n>1 else "not aprime")      #prime in one line
 
-
-
 # l=[1,2,3,4,5]
 # print("unique" if len(l)==len(set(l)) else "not unique")              #checking unique of list
-
-
 
 # import math
 # is_strong_number = lambda n: n == sum(math.factorial(int(digit)) for digit in str(n))    #STRONG NUMBER
 # print(is_strong_number(int(input("enter anumber :"))))
-
-
 
 
 # l=[3,1,3,2,1,3,5,2,7,4,7,3,4,5,6,8,9,3,5,8]      # geting 1st max and 2nd max numbers
