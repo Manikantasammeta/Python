4872271acef799788df09bfae815c0b6445db774
Manikantasammeta <manikantasammeta999@gmail.com>
2024-11-08 15:54:07 +0530

    Done

--- two_lines_prg.py
+++ two_lines_prg.py
@@ -12,6 +12,10 @@
 # print(is_strong_number(int(input("enter anumber :"))))
 
 
+
+
+
+
 # l=[3,1,3,2,1,3,5,2,7,4,7,3,4,5,6,8,9,3,5,8]      # geting 1st max and 2nd max numbers
 
 # print(list(set(l))[-1],list(set(l))[-2])
