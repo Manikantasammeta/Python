50c949e83db640b3af3ff95f3742f7594a29d126
Manikantasammeta <manikantasammeta999@gmail.com>
2024-11-18 17:49:21 +0530

    Done

---
+++ Vcube4.py
@@ -0,0 +1,11 @@
+n="12345"
+for i in range(len(n)):
+    for j in range(len(n)):
+        if i==j or i+j==len(n)-1:
+            print(n[j],end=" ")
+            
+        else:
+            print(" ",end=" ")
+    print()
+    
+    
