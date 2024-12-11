39486220b0b7139685a5090d5733fc83b985eb1a
Manikantasammeta <manikantasammeta999@gmail.com>
2024-12-11 16:32:11 +0530

    done

--- list_based2.py
+++ list_based2.py
@@ -3,15 +3,6 @@
 #                THE RESPECTIVE OUTPUT WILL BE DISPLAYED AT THE BOTTOM OF THE CODE. CHECK IT ONCE.
 
 # L=["rama","raju","raghu","mani","sita"]
-
-
-
-
-
-
-
-
-
 # print([ i[0].upper()+":"+i for i in L])
 
 #Output ['R:rama', 'R:raju', 'R:raghu', 'M:mani', 'S:sita']   #convertintg the list into list
